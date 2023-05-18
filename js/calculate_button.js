4d4159cf6d16b57c364d80ee47a0e896049117df
Elena <lpoluesova@mail.ru>
2023-05-18 20:37:17 +0500

    Added a calorie counter

--- js/calculate_button.js
+++
@@ -1,8 +0,0 @@
-/*const pristine = new Pristine(uploadForm, {
-    classTo: 'img-upload__field-wrapper',
-    errorTextParent: 'img-upload__field-wrapper',
-    errorTextClass: 'img-upload__field-wrapper__error-text'
-  });*/
-
-const calculateButton = document.querySelector('form__submit-button');
-calculateButton.disabled = false
---
+++ js/calculate_form.js
@@ -0,0 +1,47 @@
+import { calculateCaliries } from './calorie_calculation.js';
+
+const counterForm = document.querySelector('.counter__form');
+const counterResult = document.querySelector('.counter__result');
+const calculateButton = counterForm.querySelector('.form__submit-button');
+const inputElements = counterForm.querySelectorAll('.input__wrapper');
+const resetButton = counterForm.querySelector('.form__reset-button');
+
+const userData = {
+    gender: 'male',
+    age: '',
+    height: '',
+    weight: '',
+    activity: 'min'
+};
+
+
+const calorieCounterMain = () => {
+    inputElements.forEach((inputElement) => {
+        const inputItem = inputElement.querySelector('input');
+        inputItem.addEventListener('change', () => {
+            userData[inputItem.name] = inputItem.value;
+            resetButton.disabled = ![userData.age, userData.height, userData.height].some((i) => i.length > 0);
+            calculateButton.disabled = !Object.values(userData).every((i) => i.length > 0);
+        });
+    });
+
+    counterForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        counterResult.classList.remove('counter__result--hidden');
+        calculateCaliries(userData);
+    });
+
+    resetButton.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        document.getElementById('gender-male').checked = true;
+        inputElements.forEach((inputElement) => {
+            const inputItem = inputElement.querySelector('input');
+            inputItem.value = ''
+        });
+        document.getElementById('activity-minimal').checked = true;
+        resetButton.disabled = true;
+        calculateButton.disabled = true;
+    });
+};
+
+export { calorieCounterMain };
